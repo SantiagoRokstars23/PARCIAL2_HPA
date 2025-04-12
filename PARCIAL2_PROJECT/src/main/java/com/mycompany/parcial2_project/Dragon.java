372bbf83d8fbb771c16db552227e44e080d79a5d
Ronny Gutierrez <ronny98102153626@gmail.com>
2025-04-11 19:46:10 -0500

    Se actualiza la clase Dragon

--- PARCIAL2_PROJECT/src/main/java/com/mycompany/parcial2_project/Dragon.java
+++ PARCIAL2_PROJECT/src/main/java/com/mycompany/parcial2_project/Dragon.java
@@ -8,6 +8,29 @@ package com.mycompany.parcial2_project;
  *
  * @author ronny
  */
-public class Dragon {
-    
+public class Dragon extends Criatura implements Volador {
+    private String escamas;
+    private Arma arma;
+
+    public Dragon(String nombre, int salud, int fuerza, String escamas, Arma arma) {
+        super(nombre, salud, fuerza);
+        this.escamas = escamas;
+        this.arma = arma;
+    }
+
+    @Override
+    public void volar() {
+        System.out.println(nombre + " está volando.");
+    }
+
+    @Override
+    public void aterrizar() {
+        System.out.println(nombre + " aterrizó.");
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza + arma.getDañoAdicional();
+        objetivo.defender(daño);
+    }
 }
