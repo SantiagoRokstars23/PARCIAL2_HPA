9161c5aaadafc4f93cd63a24b7fb9492990bde41
SantiagoRokstars23 <sgruesomunoz@gmail.com>
2025-04-11 19:12:18 -0500

    Se crea proyecto

---
+++ PARCIAL2_PROJECT/src/main/java/com/mycompany/parcial2_project/PARCIAL2_PROJECT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial2_project;
+
+/**
+ *
+ * @author sgrue
+ */
+public class PARCIAL2_PROJECT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
